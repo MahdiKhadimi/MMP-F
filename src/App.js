667739100a5bf66b59541ef7c1e13e311745f1d4
Mahdi Khadimi <mahdikhadimi14@gmail.com>
2025-05-06 21:38:02 +0430

    Initial project setup with Tailwind and folder structure.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="h-screen w-full flex items-center justify-center text-2xl text-blue-600">
+        Microloan Portal 🚀
+      </div>
+    </>
+  );
+}
+
+export default App;
